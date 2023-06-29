3a35e4dae3ec237f8a52ffc62cc7c81901201ebd
Saja <anonimatoenmascaradog21@gmail.com>
2023-06-29 15:10:22 -0300

    fixed funtions

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ function cardsItemsLarge(items){
     items.forEach(item => {
         let card = document.createElement("div");
         card.className = "card";
-        card.innerHtml = `<div class="card" style="max-width: 540px;">
-                        <div class="row">
-                        <div class="col img">
-                            <img src="${item.img}" class="img-fluid rounded-start" alt="...">
+        card.innerHTML = `<div class="card mb-3" style="max-width: 540px;">
+                        <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="${item.img}" class="img-fluid rounded-start" alt="${item.name}" style="max-width: 150px;">
                         </div>
-                        <div class="col text">
+                        <div class="col-md-8">
                             <div class="card-body">
                             <h5 class="card-title">${item.name}</h5>
                             <p class="card-desc">${item.description}</p>
                             <p class="card-price"><small class="text-body-secondary">AR$${item.price}</small></p>
+                            <button class="back-button">Back<button>
                             </div>
                         </div>
                         </div>
@@ -26,9 +27,9 @@ function cardItemsSmall(items){
     items.forEach(item => {
     let card = document.createElement("div");
         card.className = "card";
-        card.innerHtml =`
-                        <div class="card" style="width: 18rem;">
-                            <img src="${item.img}" class="card-img-top" id="${item.id}" alt="150px">
+        card.innerHTML =`
+                        <div class="card" style="width: 180px; gap: 30px; display: flex; flex-direction: column; align-items: center; justify-content: center; margin: 10px">
+                            <img src="${item.img}" class="card-img-top" id="${item.id}" alt="${item.name}" style="width: 150px;">
                             <div class="card-body">
                                 <p class="card-text">${item.description}</p>
                             </div>
@@ -47,14 +48,27 @@ function loadItems(array){
 let cardFocus = () => {
     const addCards = document.querySelectorAll(".card-img-top");
     if (addCards !== null) {
-        for (let card of addCards) {
-            card.addEventListener("click", (event) => {
-                const prod = event.target.id;
-                cardsItemsLarge(prod);
-            });
-        }; 
-    };
-};
+      for (let card of addCards) {
+        card.addEventListener("click", (event) => {
+          const clickedImage = event.target;
+          const id = clickedImage.id;
+          const selectedItem = items.find(item => item.id == id);
+          if (selectedItem) {
+            div.innerHTML = ""; // Clear the previous cards
+            cardsItemsLarge([selectedItem]);
+          }
+        });
+      }
+    }
+  };
+
+//   function activateBackButton() {
+//     const addButtons = document.querySelectorAll(".bacl-button");
+//     addButtons.addEventListener("click", (e) => {
+
+//     })
+//   }
+  
 
 cardItemsSmall(items);
 cardFocus();
