223b0028469b4b129a59c42c611dbcb3c5a60b90
Inej <114262373+Inej-Saja-Diaspro@users.noreply.github.com>
2023-06-28 23:49:16 -0300

    Update language.js
    Updated for stock

--- language.js
+++ language.js
@@ -12,16 +12,19 @@ var currentLanguage = 'en';
                 en: {
                     subtitle: 'Cosplay & clothing Workshop',
                     description: 'I realize dreams',
+                    stock: 'Items in stock',
                 },
                 es: {
                     subtitle: 'Taller de cosplays & ropa',
                     description: 'Yo cumplo sueños',
+                    stock: 'Items en stock',
                 }
             };
             
             // Apply translations to HTML elements
             document.getElementById('subtitle').innerText = translations[currentLanguage].subtitle;
             document.getElementById('description').innerText = translations[currentLanguage].description;
+            document.getElementById('stock').innerText = translations[currentLanguage].stock;
         }
         
         // Initial data load
